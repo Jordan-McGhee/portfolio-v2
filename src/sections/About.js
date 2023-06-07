c20abcae57777a56cbbcaf4ea78f0908d3128487
Jordan McGhee <jordanmcghee95@gmail.com>
2023-06-07 17:04:01 -0400

    Added projects in json format

--- src/sections/About.js
+++ src/sections/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
 
     return (
-        <div className="text-white text-5xl">
+        <div className="text-white text-5xl leading-relaxed">
             <p>About a year into Covid, I decided I needed a career change and landed on my goal of becoming a full-time software developer. My journey started out with free python tutorials and quickly turned into enrolling into a <span>coding bootcamp</span> to accelerate my learning process.</p>
             <p className="my-16">Fast forward to today, and I am a full stack web developer proficient in Python, JavaScript, Django, React, SQL/PostgreSQL, MongoDB, and of course, HTML and CSS.</p>
             <p>I also have an extensive background in design, and have worked with the Adobe Creative Suite for 5+ years as a Sr. Digital Designer. I am constantly expanding my skillset, and happily take on new challenges.</p>
