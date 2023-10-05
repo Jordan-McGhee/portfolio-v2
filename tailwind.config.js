c037c4f2dc29ee016962de36dafcd253f03aa566
Jordan McGhee <jordanmcghee95@gmail.com>
2023-10-05 14:32:31 -0400

    Restarting portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      'primary': '#15062C',
+      'secondary': '#F6F0FF',
+      'accent': '#7A32E6',
+      'white': '#ffffff',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
