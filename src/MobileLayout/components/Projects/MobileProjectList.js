fc27c8d49edcedb250b8320bd7d9357e0e07d6df
Jordan McGhee <jordanmcghee95@gmail.com>
2023-10-22 14:28:59 -0400

    More work on mobile project components

--- src/MobileLayout/components/Projects/MobileProjectList.js
+++ src/MobileLayout/components/Projects/MobileProjectList.js
@@ -1,7 +1,27 @@
 import React from "react";
+import MobileProjectItem from "./MobileProjectItem";
 
 const MobileProjectList = props => {
+    const content = props.projects.map((project) =>
+        <MobileProjectItem
+            key = { project.name }
+            id = { project.name }
+            index = { props.projects.indexOf(project) }
+            name = { project.name }
+            about = { project.about }
+            stack = { project.stack }
+            link = { project.link ? project.link : null }
+            youtube = { project.youtube ? project.youtube : null}
+            github = { project.github }
+            image = { project.image ? project.image : null }
+        />
+    )
 
+    return (
+        <ul>
+            { content }
+        </ul>
+    )
 }
 
 export default MobileProjectList
